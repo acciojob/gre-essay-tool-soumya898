09e18a337d2756f830488408b66f04945440b33e
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-01-28 21:32:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+ <script>
+    function updateWordCount() {
+      var text = document.getElementById("evaluatedText").value;
+      var wordCount = text.split(" ").length;
+      document.getElementById("wordCount").innerHTML = wordCount;
+    }
+  </script>
